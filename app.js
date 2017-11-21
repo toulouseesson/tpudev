771730e534aa48934c1d6f6e39289d498905f3a5
damien <d.choquet@hotmail.com>
2017-11-21 12:20:55 +0100

    Test PopUp images echec

--- app.js
+++ app.js
@@ -149,4 +149,15 @@
   'Votre commentaire à été envoyé avec succès !',
   'success'
 )
-  }
+  };
+function sweet() {
+  swal({
+  title: 'Sweet!',
+  text: 'Modal with a custom image.',
+  imageUrl: 'https://unsplash.it/400/200',
+  imageWidth: 400,
+  imageHeight: 200,
+  imageAlt: 'Custom image',
+  animation: true
+})
+};
