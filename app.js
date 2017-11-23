f54c7bf004a6671a815f464f5f6aaabb5d068053
damien <d.choquet@hotmail.com>
2017-11-23 10:44:07 +0100

    changement lien vers example php au lieu de example html dans la fonction signout

--- app.js
+++ app.js
@@ -172,7 +172,7 @@ function signout() {
   confirmButtonText: 'Oui je veux me déconnecter.'
 }).then(function (result) {
   if (result.value) {
-      window.location = 'example.html';
+      window.location = 'example.php';
   }
 })
 };
